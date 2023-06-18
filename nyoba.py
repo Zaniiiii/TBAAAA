3cff1ee759d51d4e038c2ff08879ab25717c4d03
Hamdan Azani <96063759+Zaniiiii@users.noreply.github.com>
2023-06-18 15:29:45 +0700

    Update nyoba.py

--- nyoba.py
+++ nyoba.py
@@ -2,7 +2,7 @@ def transition_tab(transition):
     var = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     angka = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
     operator = ['+', '-', '*', '/', '%', '=', '<', '>']
-    indecrement = ['++','--']
+    indecrement = ['+','--']
     hasil = []
     
     # Syntax for
@@ -15,7 +15,7 @@ def transition_tab(transition):
     for i in var:
         transition[('q3', 'x')] = 'q4'
     for i in indecrement:
-        transition[('q4', '++')] = 'ACCEPT'
+        transition[('q4', '+')] = 'ACCEPT'
     # transition[('q3', ' ')] = 'q3'
     # transition[('q3', ' ')] = 'q3'
     # transition[('q3', ' ')] = 'q3'
