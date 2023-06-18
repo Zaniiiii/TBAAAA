aef308ca398758215b2906eb5ef47ce1e30c23a4
Hamdan Azani <96063759+Zaniiiii@users.noreply.github.com>
2023-06-18 16:12:53 +0700

    Update nyoba.py

--- nyoba.py
+++ nyoba.py
@@ -2,7 +2,7 @@ def transition_tab(transition):
     var = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     angka = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
     operator = ['+', '-', '*', '/', '%', '=', '<', '>']
-    indecrement = ['+','--']
+    indecrement = ['+','-']
     hasil = []
     
     # Syntax for
@@ -13,16 +13,16 @@ def transition_tab(transition):
     transition[('q2', '{')] = 'q3'
     transition[('q3', ' ')] = 'q3'
     for i in var:
-        transition[('q3', 'x')] = 'q4'
-    for i in indecrement:
-        transition[('q4', '+')] = 'q5'
-        for i in indecrement:
-            transition[('q5', '+')] = 'ACCEPT'
-    transition[('q4', ' ')] = 'q4'
+        transition[('q3', i)] = 'q4'
     for i in operator:
-        transition[('q4', '+')] = 'q6'
-    transition[('q6', ' ')] = 'q6'
-    transition[('q6', "1")] = 'ACCEPT'
+        transition[('q4', i)] = 'q5'
+    transition[('q5', '+')] = 'q6'
+    transition[('q6', ';')] = 'ACCEPT'
+    transition[('q5', ' ')] = 'q5'
+    for i in var:
+        transition[('q5', i)] = 'q7'
+    transition[('q7', ';')] = 'ACCEPT'
+#     transition[('q6', "1")] = 'ACCEPT'
     # transition[('q3', ' ')] = 'q3'
     # transition[('q3', ' ')] = 'q3'
     # transition[('q3', ' ')] = 'q3'
