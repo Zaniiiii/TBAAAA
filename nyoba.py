60ca75344ea55d18d6f97a81fde16d68a027e52d
Hamdan Azani <96063759+Zaniiiii@users.noreply.github.com>
2023-06-18 15:29:24 +0700

    Update nyoba.py

--- nyoba.py
+++ nyoba.py
@@ -14,9 +14,8 @@ def transition_tab(transition):
     transition[('q3', ' ')] = 'q3'
     for i in var:
         transition[('q3', 'x')] = 'q4'
-    transition[('q4', ' ')] = 'ACCEPT'
-#     for i in indecrement:
-#         transition[('q4', '++')] = 'ACCEPT'
+    for i in indecrement:
+        transition[('q4', '++')] = 'ACCEPT'
     # transition[('q3', ' ')] = 'q3'
     # transition[('q3', ' ')] = 'q3'
     # transition[('q3', ' ')] = 'q3'
