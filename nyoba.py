d7102ad5e14025edf9d2bf9445eb41da768b51af
Hamdan Azani <96063759+Zaniiiii@users.noreply.github.com>
2023-06-18 16:37:55 +0700

    Update nyoba.py

--- nyoba.py
+++ nyoba.py
@@ -1,7 +1,7 @@
 def transition_tab(transition):
     var = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     angka = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    operator = ['+', '-', '*', '/', '%', '=', '<', '>']
+    operator = ['*', '/', '%', '=', '<', '>']
     indecrementplus = ['+']
     indecrementneg = ['-']
     hasil = []
