5908e013d531f3048f5c44ea425904cfd4471552
Hamdan Azani <96063759+Zaniiiii@users.noreply.github.com>
2023-06-18 20:46:35 +0700

    Update LA.py

--- LA.py
+++ LA.py
@@ -39,7 +39,7 @@ def main():
     if st.button('Run'):
         output = ""
         try:
-            st.write(f'STATUS : Kodingan diterima')
+            st.write(f'STATUS : Running')
             if analyze(input_string):
                 st.write('TOKEN:')
                 result = [i for i in state_parse]
